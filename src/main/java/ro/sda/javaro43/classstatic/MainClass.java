4c791fe04fe370b6d0d8bb5bb15d3bed014663da
stefan1202 <stefan.durla@gmail.com>
2022-08-07 14:40:18 +0300

    static classes

--- src/main/java/ro/sda/javaro43/classstatic/MainClass.java
+++ src/main/java/ro/sda/javaro43/classstatic/MainClass.java
@@ -1,6 +1,8 @@
 package ro.sda.javaro43.classstatic;
 
 public class MainClass {
+    public static int staticVariable = 10;
+
     public static void main(String[] args) {
             MyOuterClass myOuterClass = new MyOuterClass();
             MyOuterClass outerClass= new MyOuterClass();
@@ -8,6 +10,15 @@ public class MainClass {
             myInnerClass1.printNumber();    // 5
             MyOuterClass.MyInnerClass myInnerClass2 = myOuterClass.new MyInnerClass();
             myInnerClass2.printNumber();    // 5
+        System.out.println(MainClass.staticVariable);
+        MainClass m = null;
+        System.out.println(m.staticVariable);
 
-        }
+        m.staticMethod(5);
+        MainClass.staticMethod(5);
+    }
+
+    public static void staticMethod(int a) {
+        System.out.println("StaticMethod " + a);
+    }
 }
