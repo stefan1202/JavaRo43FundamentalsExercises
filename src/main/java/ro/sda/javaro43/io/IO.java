31bfa15304a29e5d550d7dfdb8d255a2bb4fb40e
Sorin Costiuc <sorin.costiuc90@gmail.com>
2022-08-07 12:31:47 +0300

    print format

---
+++ src/main/java/ro/sda/javaro43/io/IO.java
@@ -0,0 +1,15 @@
+package ro.sda.javaro43.io;
+
+public class IO {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        double c = 3000;
+        double sum = a + b + c;
+        System.out.printf("100/3 = %.3f", 1.0/3.0);
+        System.out.println();
+        System.out.printf("100/3 = %.7e", 1.0/3.0);
+        System.out.println();
+        System.out.printf("boolean %b", 1>2);
+    }
+}
