34ebb8f21d776bb68aadec03a7cdbfcb857cc547
stefan1202 <stefan.durla@gmail.com>
2022-08-07 11:27:09 +0300

    Adding more code

--- src/main/java/ro/sda/javaro43/LocalVariable.java
+++ src/main/java/ro/sda/javaro43/LocalVariable.java
@@ -2,14 +2,45 @@ package ro.sda.javaro43;
 
 public class LocalVariable {
     private static Integer wrapperVariable;
+
     public static void main(String[] args) {
         System.out.println(wrapperVariable);
-        int i=0;
-        for (; i<10; ){
-            wrapperVariable = Integer.parseInt(i +"");
-            System.out.println(wrapperVariable);
-            i=i+3;
-        }
 
+        for (int i = 0; i < 10; ) {
+            wrapperVariable = Integer.parseInt(i + "");
+            System.out.println(wrapperVariable);
+            i = i + 3;
+        }
+        int i = 4;
+        long d = 5l;
+        int result = (int) (i + d);
+
+        float temperature = 36.4f;
+        if (temperature >= 37.0f) {
+            System.out.println("You have a fever!");
+        } else if (temperature >= 36.6f && temperature < 37.0f) {
+            System.out.println("You are healthy!");
+        } else {
+            System.out.println("You are weakened!");
+        }
+        switchExemple(5);
+        switchExemple(1);
+    }
+
+    public static void switchExemple(int i) {
+        switch (i) {
+            case 1:
+                System.out.println(1);
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println(4);
+                break;
+            // other cases
+            default:
+                System.out.println("Default");
+                break;
+        }
     }
 }
