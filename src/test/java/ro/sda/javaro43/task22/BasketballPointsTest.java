470e7d5718c9d70fc81eac2722b4ac9e64bab060
VLS <stanvasilevalentin4@gmail.com>
2022-08-14 13:12:08 +0300

    BasketBallPoints

---
+++ src/test/java/ro/sda/javaro43/task22/BasketballPointsTest.java
@@ -0,0 +1,61 @@
+package ro.sda.javaro43.task22;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BasketballPointsTest {
+    @Test
+    public void test01() {
+        assertEquals(5, BasketballPoints.points(1, 1));
+    }
+
+    @Test
+    public void test02() {
+        assertEquals(29, BasketballPoints.points(7, 5));
+    }
+
+    @Test
+    public void test03() {
+        assertEquals(100, BasketballPoints.points(38, 8));
+    }
+
+    @Test
+    public void test04() {
+        assertEquals(22, BasketballPoints.points(5, 4));
+    }
+
+    @Test
+    public void test05() {
+        assertEquals(206, BasketballPoints.points(4, 66));
+    }
+
+    @Test
+    public void test06() {
+        assertEquals(8, BasketballPoints.points(1, 2));
+    }
+
+    @Test
+    public void test07() {
+        assertEquals(7, BasketballPoints.points(2, 1));
+    }
+
+    @Test
+    public void test08() {
+        assertEquals(10, BasketballPoints.points(2, 2));
+    }
+
+    @Test
+    public void test09() {
+        assertEquals(264, BasketballPoints.points(69, 42));
+    }
+
+    @Test
+    public void test10() {
+        assertEquals(70, BasketballPoints.points(17, 12));
+    }
+
+    @Test
+    public void test11() {
+        assertEquals(62, BasketballPoints.points(13, 12));
+    }
+}
