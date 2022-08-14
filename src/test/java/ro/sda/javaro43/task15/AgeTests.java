c2dcbc978689cbaa98ef90dd083da203a545a2ec
Lucinia <lbal10@yahoo.com>
2022-08-14 12:31:26 +0300

    Count votes

---
+++ src/test/java/ro/sda/javaro43/task15/AgeTests.java
@@ -0,0 +1,40 @@
+package ro.sda.javaro43.task15;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AgeTests {
+
+
+
+        @Test
+        public void test01() {
+            assertEquals(Age.calcAge(10), 3650);
+        }
+
+        @Test
+        public void test02() {
+            assertEquals(Age.calcAge(0), 0);
+        }
+
+        @Test
+        public void test03() {
+            assertEquals(Age.calcAge(73), 26645);
+        }
+
+        @Test
+        public void test04() {
+            assertEquals(Age.calcAge(20), 7300);
+        }
+
+        @Test
+        public void test05() {
+            assertEquals(Age.calcAge(40), 14600);
+        }
+
+        @Test
+        public void test06() {
+            assertEquals(Age.calcAge(66), 24090);
+        }
+}
