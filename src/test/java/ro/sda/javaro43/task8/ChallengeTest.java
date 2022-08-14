db13db4b1381925ab7e60c32c784b2ea7637adb9
AnaMariaP10 <anamariap1006@yahoo.com>
2022-08-14 11:06:07 +0300

    Edge of Triangle

---
+++ src/test/java/ro/sda/javaro43/task8/ChallengeTest.java
@@ -0,0 +1,32 @@
+package ro.sda.javaro43.task8;
+
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+
+public class ChallengeTest {
+    @Test
+    public void test1() {
+        assertThat(Challenge.nextEdge(5, 4), is(8));
+    }
+
+    @Test
+    public void test2() {
+        assertThat(Challenge.nextEdge(8, 3), is(10));
+    }
+
+    @Test
+    public void test3() {
+        assertThat(Challenge.nextEdge(7, 9), is(15));
+    }
+
+    @Test
+    public void test4() {
+        assertThat(Challenge.nextEdge(10, 4), is(13));
+    }
+
+    @Test
+    public void test5() {
+        assertThat(Challenge.nextEdge(7, 2), is(8));
+    }
+}
